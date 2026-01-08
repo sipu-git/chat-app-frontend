0a600d78de23fc3bde173ebae0a6cabb621a6a0b
Sipu-982 <sipurana38@gmail.com>
2026-01-08 23:40:33 +0530

    code updated

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -29,7 +29,7 @@ export default function LoginPage() {
   }, [loading, user, router]);
 
   const handleLogin = ()=>{
-    window.location.href = "http://ec2-13-233-23-20.ap-south-1.compute.amazonaws.com:4000/api/auth/google/callback";
+    window.location.href = "http://ec2-13-233-23-20.ap-south-1.compute.amazonaws.com:4000/api/auth";
   }
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
